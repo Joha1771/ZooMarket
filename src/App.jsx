c1c6005c73139bcba22f473923faaf3c5c6885bb
vladossalimov111 <vladossalimov@gmail.com>
2026-04-10 21:21:15 +0500

    connected the new files such as: ProductPage and CategoryPage to the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./layouts/RootLayout";
 import HomePage from "./pages/HomePage";
+import CategoryPage from "./pages/CategoryPage";
+import ProductPage from "./pages/ProductPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
-    children: [{ index: true, element: <HomePage /> }],
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "category/:slug", element: <CategoryPage /> },
+      { path: "product/:id", element: <ProductPage /> },
+    ],
   },
 ]);
 
